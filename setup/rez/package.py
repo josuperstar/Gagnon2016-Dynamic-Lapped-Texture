3b1b4bfa95e1d495edb0e06e2aaae683059df8b2
jonathan gagnon <jgagnon@folksvfx.com>
2020-12-18 11:44:22 -0500

    addig missing variable for deadline

--- setup/rez/package.py
+++ setup/rez/package.py
@@ -4,7 +4,7 @@ uuid = "ba65a0c2-b993-4212-a965-4a701d1be618"
 
 description = "Texturing Fluid with rigid patches"
 
-version = "1.1.0"
+version = "1.1.1"
 
 
 authors = [ "Jonathan Gagnon" ]
@@ -19,5 +19,13 @@ def commands():
     #we should move this to {root}/otl
     #env.HOUDINI_OTLSCAN_PATH.append( "@/otls_^:@/otls:{root}/otl/;$HFS/houdini/otls")
     env.HOUDINI_PATH.append("{root}")
-    env.REZDEAD_DEFORMABLE_PATCHES_FLUID_VERSION.append("{root}")
+    env.REZDEAD_GAGNON2016_DYNAMIC_LAPPED_TEXTURE_VERSION.append("{root}")
+
+    #import os
+    #print('------------------------')
+    #print(os.environ['REZ_HOUDINIBIN_BASE'])
+    #prefix_PATH = os.environ['REZ_HOUDINIBIN_BASE']
+    #env.HFS.append(prefix_PATH + "/toolkit")
+    #env.Houdini_DIR.append(prefix_PATH )
+    #env.Houdini_DIR.append(prefix_PATH + "/toolkit/cmake")
 
